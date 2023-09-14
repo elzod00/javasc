ba90e804ef4f1adadff522ddca98124ad82404b1
elzod <you@.com>
2023-09-14 15:16:55 +0500

    jvscrpt

---
+++ app.js
@@ -0,0 +1,35 @@
+var myName = prompt('Введите свое имя' )
+console.log('Меня зовут',myName);
+
+
+
+var myAge = prompt('Сколько вам лет' )
+console.log('Мне',myAge);
+
+
+var exer = prompt('Сколько будет 6+3?' )
+console.log('Пример 1:','6 + 3 = 9','Ваш ответ',exer);
+
+
+
+var exer = prompt('Сколько будет 10*5?' )
+console.log('Пример 2:','10 * 5 = 50','Ваш ответ',exer);
+
+
+var exer = prompt('Сколько будет 10/5?' )
+console.log('Пример 3:','10 / 5 = 2','Ваш ответ',exer);
+
+
+var exer = prompt('Сколько будет 10%3?' )
+console.log('Пример 4:','10 % 3 = 3','Ваш ответ',exer);
+
+var end = alert('Откройте консоль для просмотра')
+
+
+var X,Y,Z, res;
+
+X= +prompt('Введите первое число')
+Y= +prompt('Введите второе число')
+X= +prompt('Введите третие число')
+res= (X + Y + X)/ 3
+console.log('среднее арифметическо ',res);
